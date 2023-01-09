ab43dd9fa27d3bf50693d6db1e127ced9c5e1fe1
keithconger <keith.conger@blackboxembedded.com>
2023-01-09 11:48:09 -0700

    Keep screen unlock when in use.

--- WunderLINQ-GoPro/AppDelegate.swift
+++ WunderLINQ-GoPro/AppDelegate.swift
@@ -22,6 +22,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // Keep screen unlocked
+        application.isIdleTimerDisabled = true
+        
         return true
     }
 
